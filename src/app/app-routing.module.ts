d288baa6ab9e477c111fb80f426c6633f64b1b5c
abibli <abrahamiba@gmail.com>
2021-03-16 03:42:56 -0700

    home screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
 import { HomeComponent } from './screens/home/home.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
-  { path:"home", component: HomeComponent },
-  /*{
+  {
     path: "", component: MainComponent, children: [
-      { path: "home",  component: HomeComponent, },]}*/
+      { path: "home", component: HomeComponent},
+    ],
+  }
 ];
 
 @NgModule({
