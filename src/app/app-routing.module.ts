a9882584214252b7bcb5198875915134f522991d
abibli <abrahamiba@gmail.com>
2021-03-29 11:34:22 -0700

    inquiries and orders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
+import { CatalogsComponent } from './screens/catalogs/catalogs.component';
 import { HomeComponent } from './screens/home/home.component';
+import { CalendarComponent } from './screens/inquiries/calendar/calendar.component';
+import { InquiriesComponent } from './screens/inquiries/inquiries.component';
+import { OrdersComponent } from './screens/inquiries/orders/orders.component';
+import { ProductsComponent } from './screens/inquiries/products/products.component';
 
 const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
   {
     path: "", component: MainComponent, children: [
-      { path: "home", component: HomeComponent},
+      { path: "home", component: HomeComponent },
+      {
+        path: "inquiries", component: InquiriesComponent, children: [
+          { path: "calendar", component: CalendarComponent },
+          { path: "orders", component: OrdersComponent },
+          { path: "products", component: ProductsComponent },
+        ],
+      },
+      { path: "catalogs", component: CatalogsComponent },
     ],
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './screens/home/home.component';
 import { MainComponent } from './components/main/main.component';
+import { InquiriesComponent } from './screens/inquiries/inquiries.component';
+import { CatalogsComponent } from './screens/catalogs/catalogs.component';
+import { OrdersComponent } from './screens/inquiries/orders/orders.component';
+import { CalendarComponent } from './screens/inquiries/calendar/calendar.component';
+import { ProductsComponent } from './screens/inquiries/products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MainComponent
+    MainComponent,
+    InquiriesComponent,
+    CatalogsComponent,
+    OrdersComponent,
+    CalendarComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
